04faec783a79f220878d9a347f7a384b06623f14
root <root@ip-192-168-10-60.ec2.internal>
2025-10-28 03:44:03 +0000

    fixed_app_working

--- Thunder/Terraform/simple_applicaiton_deploy/main.tf
+++ Thunder/Terraform/simple_applicaiton_deploy/main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "Thunder_Public_Subnet" {
   tags = {
     Name = var.Public_Subnet_names[each.key][0]
   }
-  depends_on = [aws_vpc.thunder_vpc]
+  depends_on = [ aws_vpc.thunder_vpc ]
 }
 
 resource "aws_subnet" "Thunder_Private_Subnet" {
@@ -24,7 +24,7 @@ resource "aws_subnet" "Thunder_Private_Subnet" {
   tags = {
     Name = var.Private_Subnet_Names[each.key][0]
   }
-  depends_on = [aws_vpc.thunder_vpc]
+  depends_on = [ aws_vpc.thunder_vpc ]
 }
 
 resource "aws_internet_gateway" "thunder_idw" {
@@ -32,7 +32,7 @@ resource "aws_internet_gateway" "thunder_idw" {
   tags = {
     Name = var.Inetnet_Gateway_Name
   }
-  depends_on = [aws_vpc.thunder_vpc]
+  depends_on = [ aws_vpc.thunder_vpc ]
 }
 
 resource "aws_route_table" "Public_Route_Table" {
@@ -48,14 +48,14 @@ resource "aws_route_table" "Public_Route_Table" {
   tags = {
     Name = var.Private_Route_Table_Name
   }
-  depends_on = [aws_internet_gateway.thunder_idw]
+  depends_on = [ aws_internet_gateway.thunder_idw ]
 }
 
 resource "aws_route_table_association" "Public_Route_Table_association" {
   for_each       = aws_subnet.Thunder_Public_Subnet
   subnet_id      = each.value.id
   route_table_id = aws_route_table.Public_Route_Table.id
-  depends_on     = [aws_route_table.Public_Route_Table]
+  depends_on = [ aws_route_table.Public_Route_Table ]
 }
 
 resource "aws_route_table" "Private_Route_Table" {
@@ -73,7 +73,7 @@ resource "aws_route_table_association" "Private_Route_Table_association" {
   for_each       = aws_subnet.Thunder_Private_Subnet
   subnet_id      = each.value.id
   route_table_id = aws_route_table.Private_Route_Table.id
-  depends_on     = [aws_route_table.Private_Route_Table]
+  depends_on = [ aws_route_table.Private_Route_Table ]
 }
 
 resource "aws_key_pair" "orbit_key" {
@@ -85,7 +85,7 @@ resource "aws_security_group" "orbit_server_sg" {
   tags = {
     Name = "orbit_server_sg"
   }
-  depends_on = [aws_vpc.thunder_vpc]
+  depends_on = [ aws_vpc.thunder_vpc ]
 }
 
 resource "aws_vpc_security_group_ingress_rule" "SSH_Rule" {
@@ -94,7 +94,7 @@ resource "aws_vpc_security_group_ingress_rule" "SSH_Rule" {
   from_port         = 22
   to_port           = 22
   ip_protocol       = "tcp"
-  depends_on        = [aws_security_group.orbit_server_sg]
+  depends_on = [ aws_security_group.orbit_server_sg ]
 }
 
 resource "aws_vpc_security_group_ingress_rule" "HHTPS_Rule" {
@@ -103,7 +103,7 @@ resource "aws_vpc_security_group_ingress_rule" "HHTPS_Rule" {
   from_port         = 443
   to_port           = 443
   ip_protocol       = "tcp"
-  depends_on        = [aws_security_group.orbit_server_sg]
+  depends_on = [ aws_security_group.orbit_server_sg ]
 }
 
 
@@ -113,7 +113,7 @@ resource "aws_vpc_security_group_ingress_rule" "HTTP_Rule" {
   from_port         = 80
   to_port           = 80
   ip_protocol       = "tcp"
-  depends_on        = [aws_security_group.orbit_server_sg]
+  depends_on = [ aws_security_group.orbit_server_sg ]
 }
 
 resource "aws_vpc_security_group_egress_rule" "Outbound_rule" {
@@ -122,7 +122,7 @@ resource "aws_vpc_security_group_egress_rule" "Outbound_rule" {
   from_port         = -1
   to_port           = -1
   ip_protocol       = "all"
-  depends_on        = [aws_security_group.orbit_server_sg]
+  depends_on = [ aws_security_group.orbit_server_sg ]
 }
 
 resource "aws_instance" "orbit_server" {
@@ -130,7 +130,7 @@ resource "aws_instance" "orbit_server" {
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.orbit_key.key_name
   associate_public_ip_address = "true"
-  security_groups             = [aws_security_group.orbit_server_sg.id]
+  security_groups = [ aws_security_group.orbit_server_sg.id ]
   subnet_id                   = aws_subnet.Thunder_Public_Subnet["us-east-1a"].id
   ebs_block_device {
     delete_on_termination = "true"
@@ -138,30 +138,34 @@ resource "aws_instance" "orbit_server" {
     volume_size           = 10
     volume_type           = "gp3"
   }
-  depends_on = [
+  depends_on = [ 
     aws_security_group.orbit_server_sg,
     aws_vpc_security_group_ingress_rule.SSH_Rule
-  ]
+   ]
   connection {
-    type        = "ssh"
-    host        = self.public_ip
-    port        = 22
-    user        = "ubuntu"
+    type = "ssh"
+    host = self.public_ip
+    port = 22
+    user = "ubuntu"
     private_key = file("../keys/orbit_terra")
   }
   provisioner "file" {
     source = "${path.module}/flask_app"
     destination = "/home/ubuntu/"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello from terraform web app instance'",
-      "sudo apt update && apt upgrade -y",
-      "sudo apt install python3-pip -y",
-      "sudo pip3 install flask",
-      "cd /home/ubuntu/flask_app/",
-      "sudo pip3 install flask ",
-      "sudo python3 app.py"
-    ]
-  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update && apt install -y python3-pip python3-flask
+    mkdir -p /home/ubuntu/flask_app
+    echo "from flask import Flask
+    app = Flask(__name__)
+    @app.route('/')
+    def home():
+         return 'Hello from Terraform Flask App!'
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=80)" > /home/ubuntu/flask_app/app.py
+    nohup python3 /home/ubuntu/flask_app/app.py > /home/ubuntu/app.log 2>&1 &
+    EOF
+
 }
